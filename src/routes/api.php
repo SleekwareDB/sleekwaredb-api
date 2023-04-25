fbda518cb38d96c33a0321055acb98fa460d2216
Imam Ali Mustofa <d.darkterminal@gmail.com>
2023-04-25 08:13:31 +0000

    Revert "chore: initial commit"
    This reverts commit 661873f5973a3e7ba3ae89905b2e512561a37541.

--- src/routes/api.php
+++
@@ -1,9 +0,0 @@
-<?php
-use Psr\Http\Message\ServerRequestInterface as Request;
-use Psr\Http\Message\ResponseInterface as Response;
-
-$app->get('/', function(Request $request, Response $response) {
-    $response_str = json_encode(['message' => 'Welcome to our SleekwareDB API']);
-    $response->getBody()->write($response_str);
-    return $response->withHeader('Content-Type', 'application/json');
-});
